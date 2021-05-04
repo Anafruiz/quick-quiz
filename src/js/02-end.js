758bb0488f26da7379ce763e65dc0a7e650632ee
Anadbh <anafruiz20@gmail.com>
2021-05-04 18:38:02 +0200

    Change script end.js

--- src/js/02-end.js
+++ src/js/02-end.js
@@ -1,30 +1,31 @@
 `use strict`;
-const username = document.getElementById("username");
-const saveScoreBtn = document.getElementById("saveScoreBtn");
-const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
+// const username = document.getElementById("username");
+// const saveScoreBtn = document.getElementById("saveScoreBtn");
+// const finalScore = document.getElementById("finalScore");
+// const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const MAX_HIGH_SCORES = 5;
+// const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText += `score:${mostRecentScore}`;
+// finalScore.innerText = mostRecentScore;
 
-username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.value;
-});
+// username.addEventListener("keydown", () => {
+//   saveScoreBtn.disabled = false;
+//   // saveScoreBtn.disabled = !username.value;
+// });
 
-saveHighScore = (e) => {
-  e.preventDefault();
+// saveHighScore = (e) => {
+//   e.preventDefault();
 
-  const score = {
-    score: mostRecentScore,
-    name: username.value,
-  };
-  highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
+//   const score = {
+//     score: mostRecentScore,
+//     name: username.value,
+//   };
+//   highScores.push(score);
+//   highScores.sort((a, b) => b.score - a.score);
+//   highScores.splice(5);
 
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
-};
+//   localStorage.setItem("highScores", JSON.stringify(highScores));
+//   window.location.assign("/");
+// };
