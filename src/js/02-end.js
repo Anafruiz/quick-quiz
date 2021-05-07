38e4f1fda8a82eee88881e07a4360484c5bd377b
Anadbh <anafruiz20@gmail.com>
2021-05-07 10:00:43 +0200

    Fix js

--- src/js/02-end.js
+++ src/js/02-end.js
@@ -1,31 +1,35 @@
 `use strict`;
-// const username = document.getElementById("username");
-// const saveScoreBtn = document.getElementById("saveScoreBtn");
-// const finalScore = document.getElementById("finalScore");
-// const mostRecentScore = localStorage.getItem("mostRecentScore");
+if (window.location.pathname === "/end.html") {
+  const username = document.getElementById("username");
+  const saveScoreBtn = document.getElementById("saveScoreBtn");
+  const finalScore = document.getElementById("finalScore");
+  const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-// const MAX_HIGH_SCORES = 5;
+  const MAX_HIGH_SCORES = 5;
 
-// finalScore.innerText = mostRecentScore;
+  if (finalScore) {
+    finalScore.innerHTML = `Score:${mostRecentScore}`;
+  }
 
-// username.addEventListener("keydown", () => {
-//   saveScoreBtn.disabled = false;
-//   // saveScoreBtn.disabled = !username.value;
-// });
+  username.addEventListener("keydown", () => {
+    saveScoreBtn.disabled = false;
+    // saveScoreBtn.disabled = !username.value;
+  });
 
-// saveHighScore = (e) => {
-//   e.preventDefault();
+  saveHighScore = (e) => {
+    e.preventDefault();
 
-//   const score = {
-//     score: mostRecentScore,
-//     name: username.value,
-//   };
-//   highScores.push(score);
-//   highScores.sort((a, b) => b.score - a.score);
-//   highScores.splice(5);
+    const score = {
+      score: mostRecentScore,
+      name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-//   localStorage.setItem("highScores", JSON.stringify(highScores));
-//   window.location.assign("/");
-// };
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+  };
+}
--- src/js/03-highScores.js
+++ src/js/03-highScores.js
@@ -1,1 +1,11 @@
 `use strict`;
+if (window.location.pathname === "/highscores.html") {
+  const highScoreList = document.getElementById("highScoresList");
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  const list = highScores
+    .map((score) => {
+      return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
+  highScoreList.innerHTML = list;
+}
