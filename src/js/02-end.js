ac18abda5a95057c91713de8fe9ac9266bb49889
Anadbh <anafruiz20@gmail.com>
2021-05-09 13:34:19 +0200

    try

--- src/js/02-end.js
+++ src/js/02-end.js
@@ -1,36 +1,38 @@
 `use strict`;
-const username = document.getElementById("username");
-const saveScoreBtn = document.getElementById("saveScoreBtn");
-const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
+if (window.location.pathname === "/quick-quiz/end.html") {
+  const username = document.getElementById("username");
+  const saveScoreBtn = document.getElementById("saveScoreBtn");
+  const finalScore = document.getElementById("finalScore");
+  const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const clap = document.getElementById("clap");
-const MAX_HIGH_SCORES = 5;
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  const clap = document.getElementById("clap");
+  const MAX_HIGH_SCORES = 5;
 
-if (finalScore) {
-  finalScore.innerHTML = `Score:${mostRecentScore}`;
-}
-console.log(mostRecentScore);
-if (mostRecentScore >= 40) {
-  clap.innerHTML = `<img src="../assets/images/clap.gif"/>`;
-}
-username.addEventListener("keydown", () => {
-  saveScoreBtn.disabled = false;
-  // saveScoreBtn.disabled = !username.value;
-});
+  if (finalScore) {
+    finalScore.innerHTML = `Score:${mostRecentScore}`;
+  }
+  console.log(mostRecentScore);
+  if (mostRecentScore >= 40) {
+    clap.innerHTML = `<img src="../assets/images/clap.gif"/>`;
+  }
+  username.addEventListener("keydown", () => {
+    saveScoreBtn.disabled = false;
+    // saveScoreBtn.disabled = !username.value;
+  });
 
-saveHighScore = (e) => {
-  e.preventDefault();
+  saveHighScore = (e) => {
+    e.preventDefault();
 
-  const score = {
-    score: mostRecentScore,
-    name: username.value,
+    const score = {
+      score: mostRecentScore,
+      name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
   };
-  highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
-
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
-};
+}
--- src/js/03-highScores.js
+++ src/js/03-highScores.js
@@ -1,9 +1,11 @@
 `use strict`;
-const highScoreList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const list = highScores
-  .map((score) => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
-highScoreList.innerHTML = list;
+if (window.location.pathname === "/quick-quiz/highscores.html") {
+  const highScoreList = document.getElementById("highScoresList");
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  const list = highScores
+    .map((score) => {
+      return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
+  highScoreList.innerHTML = list;
+}
