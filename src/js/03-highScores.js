1a5a840c940edb7cd814541aa77939f1781dae80
Anadbh <anafruiz20@gmail.com>
2021-05-09 13:22:59 +0200

    js

--- src/js/03-highScores.js
+++ src/js/03-highScores.js
@@ -1,11 +1,9 @@
 `use strict`;
-if (window.location.pathname === "/quick-quiz/highscores.html") {
-  const highScoreList = document.getElementById("highScoresList");
-  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-  const list = highScores
-    .map((score) => {
-      return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
-  highScoreList.innerHTML = list;
-}
+const highScoreList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const list = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
+highScoreList.innerHTML = list;
